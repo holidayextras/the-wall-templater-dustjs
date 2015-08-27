01f18ac38561b04503c3f10c24daa1fbd8f6ff2e
Marcus Cooke <marcus.j.cooke@gmail.com>
2015-08-27 17:00:04 +0100

    V bump, add helpers to help theatre seats generate.

---
+++ helpers/forEach.js
@@ -0,0 +1,22 @@
+/* jslint node: true */
+'use strict';
+
+var _ = require( 'lodash' );
+
+module.exports = function( dust ) {
+
+	/*
+	* @description Extend dustjs with a date differential helper courtesy of momentjs
+	* @param {string} toLoop
+	* @example {@_forEach toLoop=Object|Array|String /} output 3
+	*/
+
+	dust.helpers._forEach = function( chunk, context, bodies, params ) {
+		params = params || {};
+		_.forEach( params.toLoop, function( n ) {
+			chunk = chunk.render( bodies.block, context.push( n ) );
+		} );
+		return chunk;
+	};
+
+};
---
+++ helpers/removeAllRoomRatesBarCheapest.js
@@ -0,0 +1,41 @@
+/* jslint node: true */
+'use strict';
+
+var _ = require( 'lodash' );
+
+module.exports = function( dust ) {
+
+	/*
+	* @description Find cheapest hotel and strip rest from api reply.
+	* @param {string} object
+	* @example {@_forEach object=Object /}
+	*/
+
+	// This whole helper is very specific, needs to be rewritten asap.
+	dust.helpers._removeAllRoomRatesBarCheapest = function( chunk, context, bodies, params ) {
+		params = params || {};
+
+		// Use our params
+		var roomRates = params.object.linked.roomRates;
+		var packageRates = params.object.packageRates;
+		// Toss the used ones, so we can return everything else to the template
+		delete params[ 'object' ];
+		
+		var cheapestRoom = { id: 0, price: -1};
+
+		_.forEach( roomRates, function( roomRate ){
+			if( roomRate.grossPrice < cheapestRoom.price || cheapestRoom.price == -1 ) {
+				cheapestRoom = { id: roomRate.id, price: roomRate.grossPrice };
+			}
+		} );
+
+		_.forEach( packageRates, function( packageRate ) {
+			if ( packageRate.links.roomRates.ids == cheapestRoom.id ){
+				chunk = chunk.render( bodies.block, context.push( params ).push( packageRate ) );
+			}
+		} );
+
+		return chunk;
+	};
+
+};
