771e3ce4d117a1aeadc6f5e4378310024f37384e
Marcus Cooke <marcus.j.cooke@gmail.com>
2015-10-02 09:25:10 +0100

    Address PR feedback.

--- helpers/calcDistance.js
+++ helpers/calcDistance.js
@@ -22,16 +22,23 @@ module.exports = function( dust ) {
     var unit = dust.helpers.tap( params.unit, chunk, context );
     var decimalPlaces = dust.helpers.tap( params.decimalPlaces, chunk, context ) || 2;
 
-    var radlat1 = Math.PI * lat1/180;
-    var radlat2 = Math.PI * lat2/180;
-    var radlon1 = Math.PI * lon1/180;
-    var radlon2 = Math.PI * lon2/180;
+    function degreeToRadian( degree ){
+      return Math.PI * degree / 180;
+    }
+
+    var radlat1 = degreeToRadian( lat1 );
+    var radlat2 = degreeToRadian( lat2 );
+    var radlon1 = degreeToRadian( lon1 );
+    var radlon2 = degreeToRadian( lon2 );
     var theta = lon1-lon2;
-    var radtheta = Math.PI * theta/180;
+    var radtheta = degreeToRadian( theta );
     var dist = Math.sin( radlat1 ) * Math.sin( radlat2 ) + Math.cos( radlat1 ) * Math.cos( radlat2 ) * Math.cos( radtheta );
     dist = Math.acos( dist );
     dist = dist * 180/Math.PI;
     dist = dist * 60 * 1.1515;
+    
+    // k = kilometeres
+    // n = nautical miles.
     if ( unit=="K" ) { dist = dist * 1.609344 }
     if ( unit=="N" ) { dist = dist * 0.8684 }
 
