f6df6b686775ff394b2d815ac8b1617f6604a474
David Hersey <david.hersey@SB6166T1.local>
2016-04-01 11:06:05 +0100

    Rejig mapping to take sort keys and map accordingly

--- helpers/sortByMapping.js
+++ helpers/sortByMapping.js
@@ -9,32 +9,49 @@ module.exports = function( dust ) {
   * @description Sorting method that orders a list of things based on a provided map
   * @param {object} eventsObject Object to sort
   * @param {string} map Map to use to sort with
-  * @example {@_sortByMapping eventsObject=eventsReply.events map="ASHLKE, ASHWKE, ASHJBE} {/_sortByMapping} output loop of events
+  * @param {string} sortKeys - structure of response to sort on
+  * @example {@_sortByMapping sortObject=eventsReply.events sortKeys="links hotelProducts ids" map="PAUGLA, PAUFOR, PAUELM, PAUFAR"} {/_sortByMapping}
   */
 
   dust.helpers._sortByMapping = function( chunk, context, bodies, params ) {
-    var eventsObject = params.eventsObject;
-    var map = params.map.split(','); // Split string into array
+    // Object to sort
+    var sortObject = params.sortObject;
+    // Map to sort to
+    var map = params.map;
+    // Keys required for mapping
+    var keysParam = params.sortKeys.split(' ');
 
     var mappedObject = [];
 
-    // For every item in map array
+    var sortParameters = function( events, params ) {
+      // Loop over sort parameter structure passed in tpl
+      for(var i=0; i< params.length; i++) { 
+        if(!events) return null;
+        events = events[params[i]];
+      }
+      return events;
+    }
+    
+    // For every item in map array passed
     _.forEach( map, function( id ) {
       // For every event if the id = map id then add to mappedObject
-      _.forEach( eventsObject, function( event ) {
-        if ( event.id === id ) {
+      _.forEach( sortObject, function( event ) {
+        // Is the mapped product....
+        var requiredProduct = sortParameters( event, keysParam );
+        if ( _.include( id, requiredProduct ) ) {
+          // If it matches need to push this object into mappedObject
           mappedObject.push( event );
         }
       } );
     } );
 
     // Smush everything else we havn't already addedd into events
-    eventsObject = _.union( mappedObject, eventsObject );
+    sortObject = _.union( mappedObject, sortObject );
 
     // Release - the wall didn't like me deleting this, hence null.
     mappedObject = null;
 
-    _.forEach( eventsObject, function( item ) {
+    _.forEach( sortObject, function( item ) {
       chunk = chunk.render( bodies.block, context.push( item ) );
     } );
 
