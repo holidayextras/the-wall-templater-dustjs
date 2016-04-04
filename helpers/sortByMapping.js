9c2bf083ecae74095aa9b058cbc9c9c9304433d9
David Hersey <david.hersey@SB6166T1.local>
2016-04-04 10:19:13 +0100

    PR feedback

--- helpers/sortByMapping.js
+++ helpers/sortByMapping.js
@@ -7,10 +7,10 @@ module.exports = function( dust ) {
 
   /*
   * @description Sorting method that orders a list of things based on a provided map
-  * @param {object} eventsObject Object to sort
-  * @param {string} map Map to use to sort with
-  * @param {string} sortKeys - structure of response to sort on
-  * @example {@_sortByMapping sortObject=eventsReply.events sortKeys="links hotelProducts ids" map="PAUGLA, PAUFOR, PAUELM, PAUFAR"} {/_sortByMapping}
+  * @param {object} sortObject - Object to sort
+  * @param {string} map - Map to use to sort with
+  * @param {string} indexes - index structure of response to sort on
+  * @example {@_sortByMapping sortObject=packageRatesReply.packageRates indexes="links, hotelProducts, ids" map="PAUGLA, PAUFOR, PAUELM, PAUFAR"} {/_sortByMapping}
   */
 
   dust.helpers._sortByMapping = function( chunk, context, bodies, params ) {
@@ -19,24 +19,24 @@ module.exports = function( dust ) {
     // Map to sort to
     var map = params.map;
     // Keys required for mapping
-    var keysParam = params.sortKeys.split(' ');
+    var keysParam = params.indexes.split(', ');
 
     var mappedObject = [];
 
-    var sortParameters = function( events, sortparams ) {
+    var pickParameters = function( matchedobject, pickparams ) {
       // Loop over sort parameter structure passed in tpl
-      for ( var i = 0; i < sortparams.length; i++ ) {
-        if ( !events ) return null;
-        events = events[sortparams[i]];
+      for ( var i = 0; i < pickparams.length; i++ ) {
+        if ( !matchedobject ) return null;
+        matchedobject = matchedobject[pickparams[i]];
       }
-      return events;
+      return matchedobject;
     };
     // For every item in map array passed
     _.forEach( map, function( id ) {
       // Loop over the object to be sorted
       _.forEach( sortObject, function( matchObject ) {
-        var requiredProduct = sortParameters( matchObject, keysParam );
-        // Is the product in the map
+        var requiredProduct = pickParameters( matchObject, keysParam );
+        // Is the product in the map array?
         if ( _.include( id, requiredProduct ) ) {
           // If it matches need to push this object into mappedObject
           mappedObject.push( matchObject );
@@ -44,7 +44,7 @@ module.exports = function( dust ) {
       } );
     } );
 
-    // Smush everything else we havn't already addedd into events
+    // Smush everything else we havn't already addedd into sortObject
     sortObject = _.union( mappedObject, sortObject );
 
     // Release - the wall didn't like me deleting this, hence null.
