c5cb45a025e5a1c7913a16a00905621e3bff0c76
Owen Nicol <owen.nicol@SB6187T1.local>
2016-05-26 15:46:29 +0100

    linting fixes for sortByNumberWithFilter

--- helpers/sortByNumberWithFilter.js
+++ helpers/sortByNumberWithFilter.js
@@ -1,31 +1,30 @@
-
 /* jslint node: true */
 'use strict';
 
 var _ = require('lodash');
 var sortBy = require('../lib/sortBy');
 
-module.exports = function (dust) {
+module.exports = function(dust) {
 
   /*
-  * @description Sorting method that orders a list of things based on a child node, which is a number
-  * @param {object} parent Object to sort
-  * @param {string} node Child node to sort object by
-  * @example {@_sortByNumber parent=packageRatesReply.packageRates node="grossPrice"} {/_sortByNumber} output loop of parent object sorted by node
-  */
+   * @description Sorting method that orders a list of things based on a child node, which is a number
+   * @param {object} parent Object to sort
+   * @param {string} node Child node to sort object by
+   * @example {@_sortByNumber parent=packageRatesReply.packageRates node="grossPrice"} {/_sortByNumber} output loop of parent object sorted by node
+   */
 
-  dust.helpers._sortByNumberWithFilter = function (chunk, context, bodies, params) {
+  dust.helpers._sortByNumberWithFilter = function(chunk, context, bodies, params) {
     var parent = params.parent;
     var node = params.node;
     var filterArray = params.filterArray;
-    var filterValue = params.filterValue
+    //var filterValue = params.filterValue;
 
     var mappedObject = sortBy(parent, node);
     var newMappedObject = [];
-    
+
     //if filterArray exists, filter out from mappedObject 
     if (filterArray.length) {
-      newMappedObject = _.filter(mappedObject, function (x) {     
+      newMappedObject = _.filter(mappedObject, function(x) {
         //filter mapped object, removing objects from filter array
         return filterArray.indexOf(x.filterValue) < 0;
       });
@@ -34,7 +33,7 @@ module.exports = function (dust) {
     if (newMappedObject.length) {
       mappedObject = newMappedObject;
     }
-    _.forEach(mappedObject, function (item) {
+    _.forEach(mappedObject, function(item) {
       chunk = chunk.render(bodies.block, context.push(item));
     });
 
