1f8ec253a99dc96fe45a210a07f2d15e86b985bd
Owen Nicol <owen.nicol@SB6187T1.local>
2016-05-31 14:03:02 +0100

    sortByNumberWithFilter - orders by given node - and filters by given array

--- helpers/sortByNumberWithFilter.js
+++ helpers/sortByNumberWithFilter.js
@@ -7,27 +7,37 @@ var sortBy = require('../lib/sortBy');
 module.exports = function(dust) {
 
   /*
-   * @description Sorting method that orders a list of things based on a child node, which is a number
+   * @description Augmented sorting method that orders a list of things based on a child node, and then filters the result based on an object and a value. 
    * @param {object} parent Object to sort
    * @param {string} node Child node to sort object by
-   * @example {@_sortByNumber parent=packageRatesReply.packageRates node="grossPrice"} {/_sortByNumber} output loop of parent object sorted by node
+   * @param {object} filterArray - array to base the filter on
+   * @param {string} filterValue - value to check against in the filter function
+   * @param {boolean as string} exclude - if 'true', the filter function will exclude all items in the filterArray, based on the filterValue
+   * @param {boolean as string} include - if 'true', the filter function will include all items in the filterArray, based on the filterValue
+   * @example {@_sortByNumberWithFilter parent=packageRatesReply.packageRates node="grossPrice" filterArray=_brandConfig.featuredHotel filterValue="x.links.hotelProducts.ids[0]" include="true"} {/_sortByNumberWithFilter} output loop of parent object sorted by node
    */
 
   dust.helpers._sortByNumberWithFilter = function(chunk, context, bodies, params) {
     var parent = params.parent;
     var node = params.node;
     var filterArray = params.filterArray;
-    console.log('filterArray', filterArray);
-    //var filterValue = params.filterValue;
-
+    var filterValue = params.filterValue;
+    var include = params.include;
+    var exclude = params.exclude;
+   
     var mappedObject = sortBy(parent, node);
     var newMappedObject = [];
 
     //if filterArray exists, filter out from mappedObject
-    if (filterArray.length) {
+    if (filterArray.length && filterValue) {
       newMappedObject = _.filter(mappedObject, function(x) {
-        //filter mapped object, removing objects from filter array
-        return filterArray.indexOf(x.filterValue) < 0;
+        //filter mapped object, removing objects from filterArray
+        if (exclude) {
+          return filterArray.indexOf(eval(filterValue)) < 0;
+        }
+        else if (include) {
+          return filterArray.indexOf(eval(filterValue)) > -1;
+        }
       });
     }
 
