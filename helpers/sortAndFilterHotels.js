ff00459b7947eaf14771fecf0f0797f8c4481d4a
Owen Nicol <owen.nicol@SB6187T1.local>
2016-05-31 16:39:21 +0100

    Add new filter specific to hotels

--- helpers/sortAndFilterHotels.js
+++ helpers/sortAndFilterHotels.js
@@ -13,17 +13,15 @@ module.exports = function(dust) {
    * @param {object} parent Object to sort
    * @param {string} node Child node to sort object by
    * @param {object} filterArray - array to base the filter on
-   * @param {string} filterValue - value to check against in the filter function
    * @param {boolean as string} exclude - if 'true', the filter function will exclude all items in the filterArray, based on the filterValue
-   * @param {boolean as string} include - if 'true', the filter function will include all items in the filterArray, based on the filterValue
-   * @example {@_sortByNumberWithFilter parent=packageRatesReply.packageRates node="grossPrice" filterArray=_brandConfig.featuredHotel filterValue="x.links.hotelProducts.ids[0]" include="true"} {/_sortByNumberWithFilter} output loop of parent object sorted by node and filtered by filterArray
+   * @param {boolean as string} include - if 'true', the filter function will include all items in the filterArray
+   * @example {@_sortAndFilterHotels parent=packageRatesReply.packageRates node="grossPrice" filterArray=_brandConfig.featuredHotel filterValue="x.links.hotelProducts.ids[0]" include="true"} {/_sortAndFilterHotels} output loop of parent object sorted by node and filtered by filterArray
    */
 
-  dust.helpers._sortByNumberWithFilter = function(chunk, context, bodies, params) {
+  dust.helpers._sortAndFilterHotels = function(chunk, context, bodies, params) {
     var parent = params.parent;
     var node = params.node;
     var filterArray = params.filterArray;
-    var filterValue = params.filterValue;
     var include = params.include;
     var exclude = params.exclude;
 
@@ -32,15 +30,17 @@ module.exports = function(dust) {
 
     //if filterArray exists, filter out from mappedObject
     if (filterArray.length && filterValue) {
-      newMappedObject = _.filter(mappedObject, function(x) {
-        //filter mapped object, removing objects from filterArray
-        if (exclude) {
-          return filterArray.indexOf(eval(filterValue)) < 0;
-        }
-        else if (include) {
-          return filterArray.indexOf(eval(filterValue)) > -1;
-        }
-      });
+      if (exclude) {
+        newMappedObject = _.filter(mappedObject, function(x) {
+          //filter mapped object, exclude all recommended hotels from hotels list
+          return filterArray.indexOf(x.links.hotelProducts.ids[0]) < 0;
+        });
+      } else if (include) {
+        newMappedObject = _.filter(mappedObject, function(x) {
+          //filter mapped object, only keep recommended hotels
+          return filterArray.indexOf(x.links.hotelProducts.ids[0]) > -1;
+        });
+      }
     }
 
     if (newMappedObject.length) {
