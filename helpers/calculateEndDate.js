da41fa488de0890242008ecd56af3c73264c5105
Ralph Lawrence <ralph@holidayextras.com>
2016-12-09 12:31:13 +0000

    add the ability to reduce the input on the calculateEndDate helper

--- helpers/calculateEndDate.js
+++ helpers/calculateEndDate.js
@@ -4,11 +4,13 @@ var moment = require('moment');
 
 module.exports = function(dust) {
 
-  /*
+/*
   * @description Calculate a customer's check-out date based on their check-in date and the amount of nights they have booked
   * @param {string} startDate start date
   * @param {string} nights amount of nights
   * @param {string} [type=days] the measurement of time to add to
+  * @param {string} format - the moment format string for the return
+  * @param {integer} reduce - a number to reduce the nights by. E.g. tickets reduce by one, as standard 
   * @example {@_calcEndDate startDate="07/01/2015" nights="3" type="days" /} output "10/01/2015"
   */
 
@@ -22,6 +24,10 @@ module.exports = function(dust) {
     params = params || {};
     var startDate = dust.helpers.tap(params.startDate, chunk, context);
     var nights = dust.helpers.tap(params.nights, chunk, context);
+    var reduction = dust.helpers.tap(params.reduce, chunk, context);
+    if (reduction) {
+      nights = nights - reduction;
+    }
     var type = dust.helpers.tap(params.type, chunk, context) || 'days';
     var format = dust.helpers.tap(params.format, chunk, context);
     return chunk.write(moment.utc(startDate).add(nights, type).format(format));
