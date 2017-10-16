096643bf1b154ea29efdd6f4f4b3dba949a4335b
Vladimir Trifonov <trifonov.dev@gmail.com>
2017-10-16 13:05:29 +0300

    FP-2926: Unit tests added

---
+++ test/getRoomDescription.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+var dustMock;
+var chunkMock;
+
+describe('Get Room Description - Dust helpers', function() {
+  before(function(done) {
+    dustMock = {
+      helpers: {
+        tap: function(valueToSearch) {
+          return valueToSearch || null;
+        }
+      }
+    };
+
+    chunkMock = {
+      write: function(value) {
+        return value;
+      }
+    };
+
+    require('../helpers/getRoomDescription.js')(dustMock);
+    done();
+  });
+
+  it('should return "Double room - 2 Adults"', function() {
+    var params = {
+      occupancytype: 'DBL',
+      adults: 2,
+      children: 0,
+      infants: 0
+    };
+
+    expect(dustMock.helpers._getRoomDescription(chunkMock, null, null, params)).to.be.equal('Double room - 2 Adults');
+  });
+
+  it('should return "Accessible room - 2 Single Beds  with Bunk Beds and pull out bed - 2 Adults, 2 Children"', function() {
+    var params = {
+      roomdescription: 'Accessible room - 2 Single Beds  with Bunk Beds and pull out bed',
+      occupancytype: 'QUAD',
+      adults: 2,
+      children: 2,
+      infants: 0
+    };
+
+    expect(dustMock.helpers._getRoomDescription(chunkMock, null, null, params)).to.be.equal('Accessible room - 2 Single Beds  with Bunk Beds and pull out bed - 2 Adults, 2 Children');
+  });
+
+});
