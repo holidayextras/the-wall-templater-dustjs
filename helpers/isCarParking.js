4d27e4a2764754b584b307896bbb73fb3b3ffe90
silversurfer5150 <willedwards@lycos.com>
2017-11-20 12:02:40 +0000

    Helper file to check if car parking in confirmation template

---
+++ helpers/isCarParking.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function(dust) {
+
+ /*
+  * @description Detect whether the upgrade type is car parking for conditional rendering in confirmation template
+  * @param upgradeName is the name of the upgrade type which is used to check if it is car parking
+  * @example {@_checkIfCarParking upgradeName=content.name /} returns true if Car Parking otherwise false
+  */
+
+  dust.helpers._isCarParking = function(chunk, context, bodies, params) {
+    // strip the whitespace and lowercase content.name just to be safe and return true or false appropriately
+    return params.upgradeName.toLowerCase().replace(/ /g, "") === 'carparking' ? true : false;
+  };
+
+};
