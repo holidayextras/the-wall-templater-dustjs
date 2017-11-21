2dd18f2ae1d50c3568aa3a99c2ddb56a0d70c2a8
silversurfer5150 <willedwards@lycos.com>
2017-11-21 16:24:13 +0000

    correct interface definition and protect against possible undefined upgrade name

--- helpers/isCarParking.js
+++ helpers/isCarParking.js
@@ -5,12 +5,15 @@ module.exports = function(dust) {
  /*
   * @description Detect whether the upgrade type is car parking for conditional rendering in confirmation template
   * @param upgradeName is the name of the upgrade type which is used to check if it is car parking
-  * @example {@_checkIfCarParking upgradeName=content.name /} returns true if Car Parking otherwise false
+  * @example {@_isCarParking upgradeName=content.name /} returns true if Car Parking otherwise false
   */
 
   dust.helpers._isCarParking = function(chunk, context, bodies, params) {
     // strip the whitespace and lowercase content.name just to be safe and return true or false appropriately
-    return params.upgradeName.toLowerCase().replace(/ /g, "") === 'carparking' ? true : false;
+    if(typeof params != "undefined" && params.upgradeName) {
+      return (params.upgradeName.toLowerCase().replace(/ /g, "") === 'carparking');
+    }
+    return false;
   };
 
 };
