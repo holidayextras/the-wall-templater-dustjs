8d9167552cb77860404589f93b634a90aa127e49
thomas.van-de-put <thomas.van-de-put@holidayextras.com>
2018-10-15 16:38:55 +0100

    Add helper to combine party compositions from all the rooms

---
+++ helpers/getPartyCompFromRoomData.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var _ = require('lodash');
+var pluralise = require('../lib/pluralise');
+
+module.exports = function(dust) {
+
+  /*
+  * @description Get the combined party composition of all the rooms
+  */
+  dust.helpers._getPartyCompFromRoomData = function(chunk, context, bodies, params) {
+    params = params || {};
+    var adults = 0;
+    var children = 0;
+    _.forEach(params.rooms, function(room) {
+      adults += room.adults;
+      children += room.children + room.infants;
+    });
+    if (!adults) return false;
+    var composition = adults + ' ' + pluralise('Adult', adults);
+    if (children > 0) {
+      composition += ' and ' + children + ' ' + pluralise('Child', children);
+    }
+    return chunk.write(composition);
+  };
+};
